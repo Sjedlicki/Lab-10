2060bf9c36427e38a2626af8f43a02a4e8930cab
stevojed <jedi1014@comcast.net>
2019-01-24 15:32:24 -0500

    First Commit

---
+++ Lab 10/Circle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_10
+{
+    class Circle
+    {
+        private double radius;
+
+        public Circle(double r)
+        {
+            this.radius = r;
+        }
+
+        public double CalculateCircumference()
+        {
+            // Circumference = 2 * P * R
+            double circ;
+            circ = 2 * Math.PI * radius;
+            return circ;
+        }
+
+        public string CalculateFormattedCircumference()
+        {            
+            double x = Math.Round(CalculateCircumference(), 2);
+            return x.ToString();
+        }
+
+        public double CalculateArea()
+        {
+            // A = PI * (R * R)
+
+            double x = Math.PI * (radius * radius);
+            return x;
+        }
+
+        public string CalulateFormattedArea()
+        {
+            double x = Math.Round(CalculateArea(), 2);
+            return x.ToString();
+        }
+
+        private string FormatNumber(double x)
+        {
+            string stuff = x.ToString();
+            return stuff;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("\nRadius: "+ radius);
+            //Console.WriteLine("\nCircumference: " + CalculateCircumference());
+            Console.WriteLine("\nCircumference: " + CalculateFormattedCircumference());
+            //Console.WriteLine("\nArea: " + CalculateArea());
+            Console.WriteLine("\nArea: " + CalulateFormattedArea());
+        }
+    }
+}
---
+++ Lab 10/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_10
+{
+    class Program
+    {
+        public static int i = 1;
+        static void Main(string[] args)
+        {
+            bool run = true;
+            while (run == true)
+            {
+                Console.WriteLine("Welcome to the circle calulator");
+
+                Console.Write("Enter a radius: ");
+                double.TryParse(Console.ReadLine(), out double radius);
+
+                Circle c = new Circle(radius);
+
+                c.PrintInfo();
+                run = Continue();
+            }
+        }
+
+        public static bool Continue()
+        {    
+            Console.Write("\nWould you like to calculate another circle? (y/n) ");
+            string go = Console.ReadLine().ToLower();
+
+            bool run1;
+            if (go == "y")
+            {
+                i++;
+                Console.Clear();
+                run1 = true;
+            }
+            else if (go == "n")
+            {
+                Console.WriteLine("\nYou created " + i + " Circle objects");
+                Console.WriteLine("\nThanks for using our calculator\nGoodbye!\n");
+                run1 = false;
+            }
+            else
+            {
+                Console.WriteLine("Invalid. Try Again!");
+                run1 = Continue();
+            }
+            return run1;
+        }
+
+    }
+}
